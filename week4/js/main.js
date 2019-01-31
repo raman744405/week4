647eafd153a3b38d3cf7b0e3e77040f0fbcd8d5f
visualstudio <visualstudio@230-PC33>
2019-01-31 11:54:48 -0500

    again changes

--- week4/js/main.js
+++ week4/js/main.js
@@ -1,1 +1,13 @@
-﻿// Your code here!
+﻿//var date = new Date()
+//alert(date)
+//console.log("today is" + date)
+//var cat = date
+//document.body.innerHTML = "<h1> the date is" + cat + " </h1>"
+//document.body.innerHTML = "<h1> the date is " + cat.getMonth() + "/" + cat.getDate() + "/" + cat.getFullYear() + " </h1> "
+
+
+//how to put this code in function called myFunction
+    function myFunction(){
+        var cat= new Date();
+        document.body.innerHTML = "<h1> the date is " + (cat.getMonth() + 1) + "/" + cat.getDate() + "/" + cat.getFullYear() + " </h1> "
+    }
